0713249cb0480dfaa520ea412386291bf0b19219
Hexalyte Technology <info@hexalyte.com>
2024-10-16 19:21:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2/promise");
 
 const mysqlPool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "200106",
+  user: "root1",
+  password: "1234",
   database: "my_database",
 });
 
